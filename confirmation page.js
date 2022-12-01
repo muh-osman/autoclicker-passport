6162215f6a537c2acc4ab5f37d020e52c6fc73be
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2022-12-01 16:59:26 +0300

    Bug fixes

--- confirmation page.js
+++ confirmation page.js
@@ -1,28 +1,34 @@
 function thirdPage() {
-  // Third page => (تثبيت)
-  document.getElementsByTagName("button")[1].click();
-  console.log("Third Steep Done");
+  try {
+    // Third page => (تثبيت)
+    document.getElementsByTagName("button")[1].click();
+    console.log("Third Steep Done");
 
-  setTimeout(() => {
-    try {
-      // (نعم)
-      document.getElementsByTagName("button")[4].click();
+    setTimeout(() => {
+      try {
+        // (نعم)
+        document.getElementsByTagName("button")[4].click();
 
-      setTimeout(() => {
-        // Payment page
-        if (document.querySelectorAll("img").length === 4) {
-          soundSuccess.play();
-          console.log("ALL Done (نعم)");
-        } else {
-          thirdPage();
-          console.log("try again..");
-        }
-      }, 2000);
-    } catch {
+        setTimeout(() => {
+          // Payment page
+          if (document.querySelectorAll("img").length === 4) {
+            soundSuccess.play();
+            console.log("ALL Done (نعم)");
+          } else {
+            thirdPage();
+            console.log("try again..");
+          }
+        }, 3000);
+      } catch {
+        thirdPage();
+        console.log("slow internet, try again..");
+      }
+    }, 1000);
+  } catch {
+    setTimeout(() => {
       thirdPage();
-      console.log("slow internet, try again..");
-    }
-  }, 1000);
+    }, 2000);
+  }
 }
 
 thirdPage();
